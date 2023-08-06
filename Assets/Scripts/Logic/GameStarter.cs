5648a17e54c259ef5e6c510c792e7fdf413d7b20
Be4Die <misha375375@gmail.com>
2023-08-07 02:10:42 +0400

    диалоги, меню, стартовый уровень

---
+++ Assets/Scripts/Logic/GameStarter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    [SerializeField] private string _startSceneName;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(_startSceneName);
+    }
+}
---
+++ Assets/Scripts/Logic/StartLevel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartLevel : MonoBehaviour
+{
+    [SerializeField] private DialogueSystem _dialogueSystem;
+    [SerializeField] private string _gameplaySceneName;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _dialogStartTime;
+    [SerializeField] private float _endTime;
+
+    private float _dialogTimer;
+    private bool _isStart;
+
+    private void Start()
+    {
+        _dialogueSystem.Completed += () => StartCoroutine(EndRoutine());
+
+        _animator.SetTrigger("In");
+    }
+
+    private void Update()
+    {
+        DialogueTimer();
+    }
+    private void DialogueTimer()
+    {
+        if (_isStart)
+            return;
+        if (_dialogTimer >= _dialogStartTime)
+        {
+            _dialogueSystem.Next();
+            _isStart = true;
+        }
+        else
+            _dialogTimer += Time.deltaTime;
+    }
+
+    private IEnumerator EndRoutine()
+    {
+        _animator.SetTrigger("Out");
+        yield return new WaitForSeconds(_endTime);
+        SceneManager.LoadScene(_gameplaySceneName);
+    }
+}
