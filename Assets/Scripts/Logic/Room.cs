379480abd16021e2d8069c584d44195f129a2ab1
vavner <vavnerwork@gmail.com>
2023-08-07 14:10:49 +0600

    Fix Room

--- Assets/Scripts/Logic/Room.cs
+++ Assets/Scripts/Logic/Room.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    [SerializeField] private List<UnitBehaviour> _enemies;
+    [SerializeField] public List<UnitBehaviour> _enemies;
     [SerializeField] private Door _entryDoor;
     [SerializeField] private Door _exitDoor;
+    [SerializeField] private Room _nextRoom = null;
 
     private void Update()
     {
         foreach (var enemy in _enemies)
         {
-            if (enemy!=null)
+            if (enemy != null)
             {
                 return;
             }
@@ -21,8 +22,16 @@ public class Room : MonoBehaviour
 
     private void OpenExitDoor()
     {
+        if (_nextRoom != null)
+        {
+            foreach (var enemy in _nextRoom._enemies)
+            {
+                if (enemy != null)
+                {
+                    enemy.GetComponent<Turret>().CanActivate = true;
+                }
+            }
+        }
         _exitDoor.Open();
     }
-
-
 }
